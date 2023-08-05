465fb12174abd485257820e50b28a4b16eab8609
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-08-05 21:29:17 +0530

    first commit

---
+++ BasicGradeCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP_Polymorphism
+{
+    public class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int getTotalMarks()
+        {
+            return totalMarks;
+        }
+    }
+}
